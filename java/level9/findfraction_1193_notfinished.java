fab3e6cd4eb569880cee4bc685f8837e401cb627
devdinist <59475994+devdinist@users.noreply.github.com>
2020-02-06 21:37:39 +0900

    Update findfraction_1193_notfinished.java

--- java/level9/findfraction_1193_notfinished.java
+++ java/level9/findfraction_1193_notfinished.java
@@ -1,14 +1,41 @@
-
 package java.level9;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class findfraction_1193_notfinished {
-    public static void main(String[] args) {
-        for (int i = 1; i < 11; i++) {
-            for (int j = (i*10)+1; j < ((i+1)*10); j++) {
-                System.out.print(j+" ");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        int faze = 1;
+        while(faze < num){
+            if((faze*(faze+1)) / 2 < num){
+                faze++;
+            }else{
+                break;
             }
-            System.out.println("");
+        }
+        int gijun = (faze*(faze+1)) / 2;
+        int a = faze % 2 == 0 ? faze : 1;
+        int b = faze % 2 == 0 ? 1 : faze;
+        while(true){
+            if (faze % 2 == 0) {
+                if (gijun == num) {
+                    System.out.println(a+"/"+b);
+                    break;
+                }else{
+                    a--; b++;
+                }
+            }else{
+                if(gijun == num){
+                    System.out.println(a+"/"+b);
+                    break;
+                }else{
+                    a++; b--;
+                }
+            }
+            gijun--;
         }
     }
 }
