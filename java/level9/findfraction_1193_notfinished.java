94c3442661553c29284ffaebede914a995fed3a6
devdinist <59475994+devdinist@users.noreply.github.com>
2020-02-06 17:50:54 +0900

    Update findfraction_1193_notfinished.java
    미완성파일

--- java/level9/findfraction_1193_notfinished.java
+++ java/level9/findfraction_1193_notfinished.java
@@ -2,7 +2,7 @@
 package java.level9;
 
 
-public class findfraction_1193 {
+public class findfraction_1193_notfinished {
     public static void main(String[] args) {
         for (int i = 1; i < 11; i++) {
             for (int j = (i*10)+1; j < ((i+1)*10); j++) {
