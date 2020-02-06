ad1837b5da5ff44fc858fbb37d66748e6c288682
devdinist <59475994+devdinist@users.noreply.github.com>
2020-02-06 15:37:50 +0900

    백준 2884번

---
+++ level2/alarm_2884.java
@@ -0,0 +1,29 @@
+package level2;
+
+
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+import java.util.Scanner;
+//import java.util.StringTokenizer;
+
+public class alarm_2884 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int h = s.nextInt();
+        int m = s.nextInt();
+        int sm = m > 45 ? m-45 : m-45+60;
+        int sh = m > 45 ? h : (h-1 > 0 ? h-1 : 23);
+        System.out.printf("%d %d",sh,sm);
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        try {
+//            StringTokenizer st = new StringTokenizer(br.readLine());
+//            int h = Integer.parseInt(st.nextToken());
+//            int m = Integer.parseInt(st.nextToken());
+//            int sm = m > 45 ? m-45 : m-45+60;
+//            int sh = m > 45 ? h : (h-1 > 0 ? h-1 : 23);
+//            System.out.printf("%d %d",sh,sm);
+//        } catch (IOException err) {
+//        }
+    }
+}
