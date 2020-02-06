673dcbf27c5b553a0245e0c68675a322dbfbc185
devdinist <59475994+devdinist@users.noreply.github.com>
2020-02-06 21:38:02 +0900

    Update findfraction_1193.java

--- java/level9/findfraction_1193.java
+++ java/level9/findfraction_1193.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class findfraction_1193_notfinished {
+public class findfraction_1193 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int num = Integer.parseInt(br.readLine());
